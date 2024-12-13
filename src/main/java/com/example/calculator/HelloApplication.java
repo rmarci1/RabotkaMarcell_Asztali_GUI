db720e6903c6e4f2da1b95b7a7f28a01b41686ba
Marcell Rabotka <rigbimarci@gmail.com>
2024-12-13 15:46:59 +0100

    Initial commit

---
+++ src/main/java/com/example/calculator/HelloApplication.java
@@ -0,0 +1,30 @@
+package com.example.calculator;
+
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+
+
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ src/main/java/com/example/calculator/HelloController.java
@@ -0,0 +1,93 @@
+package com.example.calculator;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class HelloController {
+
+
+    @FXML
+    private TextField first_input;
+    @FXML
+    private TextField second_input;
+    @FXML
+    private Label number;
+
+    private String operation;
+    @FXML
+    protected void Onplus(ActionEvent actionEvent) {
+        operation = "plus";
+        calculate();
+    }
+    @FXML
+    protected void Onminus(ActionEvent actionEvent) {
+        operation = "minus";
+        calculate();
+    }
+    @FXML
+    protected void Onmultiply(ActionEvent actionEvent) {
+        operation = "multiply";
+        calculate();
+    }
+    @FXML
+    protected void Ondivide(ActionEvent actionEvent) {
+        operation = "divide";
+        calculate();
+    }
+    @FXML
+    protected void Onpercentage(ActionEvent actionEvent) {
+        operation = "percentage";
+        calculate();
+    }
+    private void calculate(){
+        double cal = 0;
+        System.out.println(operation);
+        try{
+            if(first_input.getText().isEmpty() || second_input.getText().isEmpty()){
+                throw new IllegalArgumentException("All fields must be filled");
+            }
+        }
+        catch (NumberFormatException e){
+            showAlert("Invalid input","Please enter valid numbers");
+        }
+        catch(IllegalArgumentException | ArithmeticException e){
+            showAlert("Error",e.getMessage());
+        }
+        switch (operation){
+            case "plus" : {
+                cal = Double.parseDouble(first_input.getText()) + Double.parseDouble(second_input.getText());
+                break;
+            }
+            case "minus": {
+                cal = Double.parseDouble(first_input.getText()) - Double.parseDouble(second_input.getText());
+                break;
+            }
+            case "multiply": {
+                cal = Double.parseDouble(first_input.getText()) * Double.parseDouble(second_input.getText());
+                break;
+            }
+            case "divide" : {
+                if(Double.parseDouble(second_input.getText()) == 0){
+                    throw  new ArithmeticException("Cannot divide by zero.");
+                }
+                cal = Math.round((Double.parseDouble(first_input.getText()) / Double.parseDouble(second_input.getText())*100.0)/100.0);
+                break;
+            }
+            case "percentage" : {
+                cal = Double.parseDouble(first_input.getText()) % Double.parseDouble(second_input.getText());
+                break;
+            }
+        }
+        number.setText(String.valueOf(cal));
+
+    }
+    private void showAlert(String title, String message){
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle(title);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+}
